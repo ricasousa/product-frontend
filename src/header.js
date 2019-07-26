8e1ed85eb91efd2ef73553692c49e6b70e5f196f
Ricardo Sousa <ricasousa@outlook.com>
2019-07-26 08:01:08 -0300

    Added buttons on header to test functions calls

--- src/header.js
+++ src/header.js
@@ -1,14 +1,36 @@
 import React from 'react';
+import './assets/header.css';
 
 const style = {
-  color: '#cc0000'
-}
+  color: '#cc0000',
+  background: '#000',
+  width: '100px',
+  height: '30px',
+  border: 'none',
+  outline: 'none'
+};
 
-const name = 'Josh Perez';
-const element = <h1 style={style}>Hello, {name}</h1>;
+const _click = a => {
+  console.log('_click!', a);
+};
+
+const _clickNoParams = () => {
+  console.log('_clickNoParams!');
+};
 
 function Header() {
-  return element;
+  return (
+    <div className="header-container">
+      Algum texto..
+      <button style={style} onClick={() => _click('qualqeur coisa')}>
+        Click Me!
+      </button>
+
+      <button style={style} onClick={() => _clickNoParams()}>
+        Click Me 2!
+      </button>
+    </div>
+  );
 }
 
 export default Header;
