fc9176af20e7c229c11a41c895ba2d461d3dea3a
Ricardo Sousa <T3003RCR>
2019-11-05 07:38:08 -0300

    Added some files to play

--- src/App.js
+++ src/App.js
@@ -3,29 +3,20 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './header';
 import Footer from './footer';
+import LoginImage from './feature/login/components/login-image/login-image';
+import LoginForm from './feature/login/components/form/form';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="app">
+      <div className="login-image">
+        <LoginImage />
         <Header />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p className="minha-classe">
-          React Playground Project...
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <Footer description='Que lindo...' />
+      </div>
+      <div className="login-form">
+        <LoginForm />
+        <Footer description="Que lindo..." />
+      </div>
     </div>
   );
 }
